c890b5bdfa450844785f62a6cd6835bde42cf6f3
pavanteli6 <91552241+pavanteli6@users.noreply.github.com>
2024-02-06 14:18:02 +0530

    Added Login Test cases (#3)
    * first commit pavan project wait and approve process
    
    * updated e2e file with before each block
    
    * folder restructured

--- cypress/pages/loginPage.js
+++ cypress/pages/loginPage.js
@@ -1,34 +1,39 @@
 export class loginPage{
 
-    webLocators={
-        myAccount:"a[title='My Account'] span[class='hidden-xs hidden-sm hidden-md']",
-        loginButton:"li[class='dropdown open'] li:nth-child(2) a:nth-child(1)",
-        email:"#input-email",
-        password:"#input-password",
-        loginSubmitButton:"input[value='Login']"
-    }
+        myAccount = "a[title='My Account'] span[class='hidden-xs hidden-sm hidden-md']"
+        loginButton = "li[class='dropdown open'] li:nth-child(2) a:nth-child(1)"
+        email = "#input-email"
+        password = "#input-password"
+        loginSubmitButton = "input[value='Login']"
+        loginSuccessBtn = "My Orders"
 
     openURL(){
         cy.visit(Cypress.env('URL'))
     }
 
     clickOnMyAccount(){
-        cy.get(this.webLocators.myAccount).click()
+        cy.get(this.myAccount).click()
     }
 
     clickOnLoginButton(){
-        cy.get(this.webLocators.loginButton).click()
+        cy.get(this.loginButton).click()
     }
 
     enterEmail(userEmail){
-        cy.get(this.webLocators.email).type(userEmail)
+        cy.get(this.email).type(userEmail)
     }
 
     enterPassword(userPassword){
-        cy.get(this.webLocators.password).type(userPassword)
+        cy.get(this.password).type(userPassword)
     }
 
     clickOnLoginSubmitButton(){
-        cy.get(this.webLocators.loginSubmitButton).click()
+        cy.get(this.loginSubmitButton).click()
+        cy.wait(2000)
+    }
+
+    loginSuccess(){
+        cy.contains(this.loginSuccessBtn)
+        
     }
 }
